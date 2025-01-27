1492d761ef3d61af1ef93f4b01d6a479cff1697d
Avimitin <dev@avimit.in>
2025-01-27 16:42:13 +0800

    [mill] migrate to mill 0.12.5

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -5,8 +5,11 @@
   circt-full = final.callPackage ./pkgs/circt-full.nix { };
   submodules = final.callPackage ./submodules.nix { };
   chisel-interface = final.callPackage ./chisel-interface.nix { };
-  mill = let jre = final.jdk21; in
-    (prev.mill.override { inherit jre; }).overrideAttrs (_: {
-      passthru = { inherit jre; };
-    });
+  mill = prev.mill.overrideAttrs {
+    version = "unstable-0.12.5-173-15dded";
+    src = final.fetchurl {
+      url = "https://github.com/com-lihaoyi/mill/releases/download/0.12.5/0.12.5-173-15dded-assembly";
+      hash = "sha256-xP59tONOu0CG5Gce4ru+st5KUH7Wcd10d/pQdELjSJM=";
+    };
+  };
 }
