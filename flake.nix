09b6c84597ffcbcb3d4b1237a4c39b27d6d63e5f
Avimitin <dev@avimit.in>
2025-01-27 14:27:49 +0800

    [nix] add chisel-nix input

--- flake.nix
+++ flake.nix
@@ -3,18 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    chisel-nix.url = "github:chipsalliance/chisel-nix";
     nixpkgs-for-circt.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-for-circt }@inputs:
+  outputs = { self, nixpkgs, chisel-nix, flake-utils, nixpkgs-for-circt }@inputs:
     let
       overlay = import ./nix/overlay.nix { inherit self; };
     in
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+          pkgs = import nixpkgs { inherit system; overlays = [ chisel-nix.overlays.mill-flows overlay ]; };
         in
         {
           legacyPackages = pkgs;
